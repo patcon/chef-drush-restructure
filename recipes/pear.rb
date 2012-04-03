391fd84b109f859e9b126ef8a0141ea4b7303399
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-03 14:22:23 -0400

    Oops. metadata #fail

--- recipes/pear.rb
+++ recipes/pear.rb
@@ -1,7 +1,7 @@
 # 
 # Author:: Mark Sonnabaum <mark.sonnabaum@acquia.com>
 # Cookbook Name:: drush
-# Recipe:: default
+# Recipe:: pear
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
